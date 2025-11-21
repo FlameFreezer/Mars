d8de134b5fc6b77c235910b75b92868ce284e58e
FFreeze <quincymon@icloud.com>
2025-11-21 12:18:39 -0800

    Device Creation

--- src/mars.c
+++ src/mars.c
@@ -1,9 +1,12 @@
 #include "mars.h"
 
 #include <stdio.h>
+#include <string.h>
 
 int const WIDTH = 800;
 int const HEIGHT = 600;
+
+char const* deviceExtensionNames[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 
 MarsError marsMakeError(enum MarsErrorType key, char const* message) {
     MarsError result;
@@ -23,7 +26,7 @@ VkBool32 debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT 		messageSeverity,
     return VK_FALSE;
 }
 
-MarsError marsCreateVkDevice(VkDevice* device, VkPhysicalDevice* physicalDevice, VkInstance const instance) {
+MarsError marsCreateVkDevice(VkDevice* device, VkPhysicalDevice* physicalDevice, VkInstance const instance, VkSurfaceKHR const surface) {
     uint32_t physicalDeviceCount = 0;
     vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, NULL);
     VkPhysicalDevice* physicalDevices = SDL_malloc(sizeof(VkPhysicalDevice) * physicalDeviceCount);
@@ -33,23 +36,90 @@ MarsError marsCreateVkDevice(VkDevice* device, VkPhysicalDevice* physicalDevice,
     if(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices) != VK_SUCCESS) {
 	return marsMakeError(MARS_ENUMERATE_PHYSICAL_DEVICES_FAIL, "Failed to enumerate physical devices!");
     }
-    //For now, just pick the first phsyical device found
-    *physicalDevice = physicalDevices[0];
+    uint32_t queueFamilyIndex;
+    uint32_t queueCount;
+    uint32_t queueFamilyPropertyCount = 0;
+
+    for(int i = 0; i < physicalDeviceCount; i++) {
+	uint32_t deviceExtensionPropertyCount = 0;
+	if(vkEnumerateDeviceExtensionProperties(physicalDevices[i], NULL, &deviceExtensionPropertyCount, NULL) != VK_SUCCESS) {
+	    return marsMakeError(MARS_MISC_ERROR, "Failed to enumerate physical device extension properties!");
+	}
+	VkExtensionProperties* extensionProperties = SDL_malloc(sizeof(VkExtensionProperties) * deviceExtensionPropertyCount);
+	if(!extensionProperties) {
+	    return marsMakeError(MARS_MEMORY_ALLOC_FAIL, "Failed to allocate host memory!");
+	}
+	if(vkEnumerateDeviceExtensionProperties(physicalDevices[i], NULL, &deviceExtensionPropertyCount, extensionProperties) != VK_SUCCESS) {
+	    SDL_free(extensionProperties);
+	    return marsMakeError(MARS_MISC_ERROR, "Failed to enumerate physical device extension properties!");
+	}
+	for(int j = 0; j < deviceExtensionPropertyCount; j++) {
+	    if(strcmp(extensionProperties[j].extensionName, deviceExtensionNames[0]) == 0) {
+		goto Queue_Check;
+	    }
+	}
+	SDL_free(extensionProperties);
+	return marsMakeError(MARS_FIND_SUITABLE_GPU_FAIL, "Failed to find a GPU supporting needed extensions!");
+
+    Queue_Check:
+	SDL_free(extensionProperties);
+	vkGetPhysicalDeviceQueueFamilyProperties2(physicalDevices[i], &queueFamilyPropertyCount, NULL);
+	VkQueueFamilyProperties2* queueFamilyProperties = SDL_malloc(sizeof(VkQueueFamilyProperties2) * queueFamilyPropertyCount);
+	if(!queueFamilyProperties) {
+	    return marsMakeError(MARS_MEMORY_ALLOC_FAIL, "Failed to allocate host memory!");
+	}
+	vkGetPhysicalDeviceQueueFamilyProperties2(physicalDevices[i], &queueFamilyPropertyCount, queueFamilyProperties);
+	for(int j = 0; j < queueFamilyPropertyCount; j++) {
+	    if(queueFamilyProperties[j].queueFamilyProperties.queueFlags & (VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT)) {
+		VkBool32 surfaceSupport;
+		if(vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevices[i], j, surface, &surfaceSupport) != VK_SUCCESS) {
+		    SDL_free(queueFamilyProperties);
+		    return marsMakeError(MARS_MISC_ERROR, "Failed to get physical device surface support!");
+		}
+		if(surfaceSupport == VK_TRUE) {
+		    queueFamilyIndex = j;
+		    queueCount = queueFamilyProperties[j].queueFamilyProperties.queueCount;
+		    *physicalDevice = physicalDevices[i];
+		    SDL_free(queueFamilyProperties);
+		    goto Device_Creation;
+		}
+	    }
+	}
+    }
+    return marsMakeError(MARS_FIND_SUITABLE_GPU_FAIL, "Failed to find a GPU with needed queue families!");
+
+Device_Creation:
     SDL_free(physicalDevices);
+
+    float* queuePriorities = SDL_calloc(queueCount, sizeof(float));
+    if(!queuePriorities) {
+	return marsMakeError(MARS_MEMORY_ALLOC_FAIL, "Failed to allocate host memory!");
+    }
+
+    VkDeviceQueueCreateInfo queueInfo = {
+	.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+	.pNext = NULL,
+	.flags = 0,
+	.queueFamilyIndex = queueFamilyIndex,
+	.queueCount = queueCount,
+	.pQueuePriorities = queuePriorities
+    };
 
     VkDeviceCreateInfo deviceInfo = {
 	.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
 	.pNext = NULL,
 	.flags = 0,
-	.queueCreateInfoCount = 0,
-	.pQueueCreateInfos = NULL,
-	.enabledExtensionCount = 0,
-	.ppEnabledExtensionNames = NULL,
+	.queueCreateInfoCount = 1,
+	.pQueueCreateInfos = &queueInfo,
+	.enabledExtensionCount = sizeof(deviceExtensionNames) / sizeof(char*),
+	.ppEnabledExtensionNames = deviceExtensionNames,
 	.pEnabledFeatures = NULL
     };
+
     if(vkCreateDevice(*physicalDevice, &deviceInfo, NULL, device) != VK_SUCCESS) {
 	return marsMakeError(MARS_DEVICE_CREATION_FAIL, "Failed to create VkDevice!");
     }
+    SDL_free(queuePriorities);
     return MARS_SUCCESS;
 }
 
@@ -62,6 +132,10 @@ MarsError marsCreateVkInstance(VkInstance* instance, char* appName) {
     }
     extNames[0] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
     memcpy(&extNames[1], sdlExtNames, extCount++ * sizeof(char*));
+    printf("Extensions\n");
+    for(int i = 0; i < extCount; i++) {
+	printf("\t%s\n", extNames[i]);
+    }
 
     VkApplicationInfo appInfo = {
 	.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
@@ -122,7 +196,7 @@ MarsError marsInitRenderer(MarsRenderer* marsRenderer, char* appName) {
     if(!SDL_Vulkan_CreateSurface(marsRenderer->window, marsRenderer->instance, NULL, &marsRenderer->surface)) {
 	return marsMakeError(MARS_SURFACE_CREATION_FAIL, SDL_GetError());
     }
-    MARS_TRY(marsCreateVkDevice(&marsRenderer->device, &marsRenderer->physicalDevice, marsRenderer->instance));
+    MARS_TRY(marsCreateVkDevice(&marsRenderer->device, &marsRenderer->physicalDevice, marsRenderer->instance, marsRenderer->surface));
     
     return MARS_SUCCESS;
 }
