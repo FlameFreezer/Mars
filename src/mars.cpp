7810d3e41389c94cf7a439e3b8b332c390008e58
FFreeze <quincymon@icloud.com>
2025-12-21 20:23:19 -0800

    Linux support: replaced std::chrono::high_resolution_clock with std::chrono::steady_clock
    Turns out on MSVC high_resolution_clock is an alias for steady_clock but on libstdc++ it is an alias for system_clock. I want to use steady_clock, so it's been changed wherever needed

--- src/mars.cpp
+++ src/mars.cpp
@@ -27,15 +27,17 @@ namespace mars {
         deinitializedLibrary = true;
     }
 
-    Game::Game() noexcept : windowName("My Mars Game"), appName("My Mars Game"), prevTime(std::chrono::high_resolution_clock::now()) {}
-    Game::Game(const std::string& name) noexcept : windowName(name), appName(name), prevTime(std::chrono::high_resolution_clock::now()) {}
+    Game::Game() noexcept : windowName("My Mars Game"), appName("My Mars Game") {}
+    Game::Game(const std::string& name) noexcept : windowName(name), appName(name) {}
     Error<noreturn> Game::init() noexcept {
         TRY(renderer.init(appName));
         return success();
     }
     Error<noreturn> Game::draw() noexcept {
-        auto const deltaTime = std::chrono::duration<std::int64_t, std::chrono::nanoseconds::period>(std::chrono::high_resolution_clock::now() - prevTime);
-        prevTime = std::chrono::high_resolution_clock::now();
+	auto const now = std::chrono::steady_clock::now();
+        auto const deltaTime = std::chrono::duration<std::int64_t, std::chrono::nanoseconds::period>(
+	    now - prevTime);
+        prevTime = now;
         TRY(renderer.drawFrame(deltaTime));
         return success();
     }
