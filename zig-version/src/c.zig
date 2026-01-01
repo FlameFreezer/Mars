b98ce16b794a32234aad46a863de1c6f5f77eebf
FFreeze <quincymon@icloud.com>
2025-12-31 21:28:54 -0800

    ZIG CODE REMOVED - I've finally surpassed all the features of the zig version, so I no longer need it for reference - Well, not exactly all of them, but its code structure was garbage and I'd rather implement the object rendering stuff differently

--- zig-version/src/c.zig
+++
@@ -1,9 +0,0 @@
-pub usingnamespace @cImport({
-    @cInclude("string.h");
-    @cDefine("SDL_HINT_VIDEO_SYNC_WINDOW_OPERATIONS", "1");
-    @cInclude("SDL3/SDL.h");
-    @cInclude("SDL3/SDL_vulkan.h");
-    @cInclude("vulkan/vulkan.h");
-    @cDefine("STB_IMAGE_IMPLEMENTATION", {});
-    @cInclude("stb_image.h");
-});
