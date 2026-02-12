e0ee522f61cabe693f27e86ec6185112a32b2fa2
FFreeze <quincymon@icloud.com>
2026-02-12 12:05:33 -0800

    Basic 2D controller!

--- app/main.cpp
+++ app/main.cpp
@@ -13,6 +13,9 @@ import mars;
 using ErrorNoreturn = mars::Error<mars::noreturn>;
 
 #define SPEED 1.0f
+#define PLAYER_SPEED 200.0f
+
+mars::ID player;
 
 void handleEvent(mars::Game& game, SDL_Event const& e) noexcept {
     switch(e.type) {
@@ -31,6 +34,7 @@ void handleEvent(mars::Game& game, SDL_Event const& e) noexcept {
 void handleKeyboardInput(mars::Game& game) noexcept {
     game.updateKeyState();
     glm::vec3 velocity(0.0f);
+    glm::vec3 playerVelocity(0.0f);
     if(game.keyState[SDL_SCANCODE_D]) {
         glm::vec3 const right(glm::normalize(glm::cross(game.camera.dir, game.camera.up)));
         velocity += right;
@@ -57,6 +61,18 @@ void handleKeyboardInput(mars::Game& game) noexcept {
     if(game.keyState[SDL_SCANCODE_LSHIFT]) {
         velocity -= game.camera.up;
     }
+    if(game.keyState[SDL_SCANCODE_RIGHT]) {
+        playerVelocity.x += 1.0f;
+    }
+    if(game.keyState[SDL_SCANCODE_LEFT]) {
+        playerVelocity.x -= 1.0f;
+    }
+    if(game.keyState[SDL_SCANCODE_DOWN]) {
+        playerVelocity.y += 1.0f;
+    }
+    if(game.keyState[SDL_SCANCODE_UP]) {
+        playerVelocity.y -= 1.0f;
+    }
     if(game.keyState[SDL_SCANCODE_ESCAPE]) {
         game.setFlags(mars::flagBits::stopExecution);
     }
@@ -64,6 +80,10 @@ void handleKeyboardInput(mars::Game& game) noexcept {
     if(velocity != glm::vec3(0.0f)) {
         velocity = glm::normalize(velocity) * SPEED * game.getDeltaTimeSeconds();
         game.camera.pos += velocity;
+    }
+    if(playerVelocity != glm::vec3(0.0f)) {
+        playerVelocity = glm::normalize(playerVelocity) * PLAYER_SPEED * game.getDeltaTimeSeconds();
+        game.objects.at(game.objects.positions, player) += playerVelocity;
     }
 }
 
@@ -88,7 +108,7 @@ void initCamera(mars::Game& game) noexcept {
 ErrorNoreturn mainLoop(mars::Game& game) noexcept {
     initCamera(game);
     mars::ID texture = game.loadTexture(std::string(MARS_ASSETS_PATH) + "S_Placeholder.png");
-    mars::ID object = game.createObject(game.shapes.square, texture, glm::vec3(0.0f), glm::vec3(100.0f));
+    player = game.createObject(game.shapes.square, texture, glm::vec3(0.0f), glm::vec3(100.0f));
     while(!game.hasFlags(mars::flagBits::stopExecution)) {
     	SDL_Event e;
         game.updateTime();
