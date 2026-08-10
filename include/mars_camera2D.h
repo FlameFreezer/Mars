01be2ac67a7f9ab90cfd38067c4c66a2e8fdc9a7
FFreeze <quincymon@icloud.com>
2026-08-10 10:58:23 -0700

    Removed movement logic from camera class
    - said movement logic should be handled by the application and not the framework

--- include/mars_camera2D.h
+++ include/mars_camera2D.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <format>
-
 #include <glm/glm.hpp>
 
-#include "mars_types.h"
 #include "error.h"
 #include "jsonparser.h"
 
@@ -12,74 +9,39 @@ namespace mars {
 	class Camera2D {
 	public:
 		friend class Camera2DBuilder;
-		enum class FollowMode : u8 {
-			DONT_FOLLOW,
-			FOLLOW_ENTITY,
-			FOLLOW_POSITION
-		};
-		enum class FollowState : u8 {
-			IDLE,
-			FOLLOWING,
-			NULL_STATE
-		};
 		//Getters
 		glm::vec2 getPosition() const noexcept;
 		glm::vec2 getScale() const noexcept;
-		glm::vec2 getDeadzone() const noexcept;
 		glm::vec2 getTargetPosition() const noexcept;
 		glm::vec2 getFollowLead() const noexcept;
 		glm::vec2 getCenter() const noexcept;
-		glm::vec2 getFollowLeadSigns() const noexcept;
-		ID getTargetID() const noexcept;
-		FollowMode getFollowMode() const noexcept;
-		FollowState getFollowState() const noexcept;
-		float getFollowSpeed() const noexcept;
-		//Setters
-		Error<noreturn> setTargetID(ID id, bool doFollowEntity = true) noexcept;
-		Error<noreturn> setTargetIDAndLookAt(ID id, bool doFollowEntity = true) noexcept;
-		Error<noreturn> setFollowMode(FollowMode mode) noexcept;
+        float getSpeed() const noexcept;
 		void setPosition(glm::vec2 pos) noexcept;
 		void setScale(glm::vec2 scale) noexcept;
-		void setDeadzone(glm::vec2 deadzone) noexcept;
 		void setTargetPosition(glm::vec2 pos) noexcept;
-		void setFollowLeadSigns(glm::vec2 signs) noexcept;
-		void setFollowSpeed(float speed) noexcept;
-		void setFollowLead(glm::vec2 lead) noexcept;
-		// Changes the position of the camera such that the argument is at the center of its viewport. Also sets the follow mode to FOLLOW_POSITION.
-		void moveAndLookAt(glm::vec2 where) noexcept;
-		// Sets the target position such that the argument is at the center of its viewport, but does not move the camera. Also sets the follow mode to FOLLOW_POSITION.
+        void setTargetCenter(glm::vec2 where) noexcept;
+        void setSpeed(float speed) noexcept;
 		void lookAt(glm::vec2 where) noexcept;
-		Error<noreturn> update(float delta) noexcept;
+		void update(float delta) noexcept;
 	private:
-		Error<FollowState> checkStateTransitions() const noexcept;
 		glm::vec2 mPosition{};
 		glm::vec2 mScale{};
-		glm::vec2 mDeadzone{};
 		glm::vec2 mTargetPosition{};
-		glm::vec2 mFollowLead{};
-		glm::vec2 mFollowLeadSigns{};
-		ID mTargetID{ nullID };
-		float mFollowSpeed{};
-		FollowMode mFollowMode{ FollowMode::DONT_FOLLOW };
-		FollowState mFollowState{ FollowState::IDLE };
+        float mSpeed{};
 	};
 
 	class Camera2DBuilder {
 	public:
 		Camera2DBuilder& setPosition(glm::vec2 pos) noexcept;
 		Camera2DBuilder& setScale(glm::vec2 scale) noexcept;
-		Camera2DBuilder& setDeadzone(glm::vec2 deadzone) noexcept;
 		Camera2DBuilder& setTargetPosition(glm::vec2 targetPos) noexcept;
-		Camera2DBuilder& setFollowLead(glm::vec2 lead) noexcept;
-		Camera2DBuilder& setFollowSpeed(float speed) noexcept;
+        Camera2DBuilder& setSpeed(float speed) noexcept;
 		Camera2D build() const noexcept;
 	private:
 		glm::vec2 mPosition{};
 		glm::vec2 mScale{};
-		glm::vec2 mDeadzone{};
 		glm::vec2 mTargetPosition{};
-		glm::vec2 mFollowLead{};
-		float mFollowSpeed{};
+        float mSpeed{};
 	};
 }
 
--- src/mars_camera2D.cpp
+++ src/mars_camera2D.cpp
@@ -10,160 +10,43 @@ namespace mars {
 	glm::vec2 Camera2D::getScale() const noexcept {
 		return mScale;
 	}
-	glm::vec2 Camera2D::getDeadzone() const noexcept {
-		return mDeadzone;
-	}
 	glm::vec2 Camera2D::getTargetPosition() const noexcept {
 		return mTargetPosition;
-	}
-	glm::vec2 Camera2D::getFollowLead() const noexcept {
-		return mFollowLead;
 	}
 	glm::vec2 Camera2D::getCenter() const noexcept {
 		return mPosition + 0.5f * mScale;
 	}
-	glm::vec2 Camera2D::getFollowLeadSigns() const noexcept {
-		return mFollowLeadSigns;
-	}
-	ID Camera2D::getTargetID() const noexcept {
-		return mTargetID;
-	}
-	Camera2D::FollowMode Camera2D::getFollowMode() const noexcept {
-		return mFollowMode;
-	}
-	Camera2D::FollowState Camera2D::getFollowState() const noexcept {
-		return mFollowState;
-	}
-	float Camera2D::getFollowSpeed() const noexcept {
-		return mFollowSpeed;
-	}
-	Error<noreturn> Camera2D::setTargetID(ID id, bool doFollowEntity) noexcept {
-		const ComponentSystem<Draw>& sysDraw = ECS::get().system<Component::draw>();
-		if (!sysDraw.has(id)) {
-			FATAL(std::format("Entity with ID {} doesn't have a draw component", id));
-		}
-		mTargetID = id;
-		if (doFollowEntity) {
-			mTargetPosition = sysDraw.position(mTargetID) + 0.5f * sysDraw.scale(mTargetID) - 0.5f * mScale;
-			mFollowMode = Camera2D::FollowMode::FOLLOW_ENTITY;
-		}
-		return SUCCESS;
-	}
-	Error<noreturn> Camera2D::setTargetIDAndLookAt(ID id, bool doFollowEntity) noexcept {
-		const ComponentSystem<Draw>& sysDraw = ECS::get().system<Component::draw>();
-		if (!sysDraw.has(id)) {
-			FATAL(std::format("Entity with ID {} doesn't have a draw component", id));
-		}
-		mTargetID = id;
-		if (doFollowEntity) {
-			mTargetPosition = sysDraw.position(mTargetID) + 0.5f * sysDraw.scale(mTargetID) - 0.5f * mScale;
-			mFollowMode = Camera2D::FollowMode::FOLLOW_ENTITY;
-		}
-		mPosition = mTargetPosition;
-		return SUCCESS;
-	}
-	Error<noreturn> Camera2D::setFollowMode(Camera2D::FollowMode mode) noexcept {
-		if (mode == Camera2D::FollowMode::FOLLOW_ENTITY and mTargetID == nullID) {
-			FATAL("Tried to set the follow mode of a Camera2D to FOLLOW_ENTITY, but the camera had no target entity");
-		}
-		mFollowMode = mode;
-		return SUCCESS;
-	}
-	void Camera2D::setPosition(glm::vec2 pos) noexcept {
-		mPosition = pos;
-		mFollowMode = Camera2D::FollowMode::DONT_FOLLOW;
-	}
-	void Camera2D::setScale(glm::vec2 scale) noexcept {
-		mScale = scale;
-	}
-	void Camera2D::setDeadzone(glm::vec2 deadzone) noexcept {
-		mDeadzone = deadzone;
-	}
+    float Camera2D::getSpeed() const noexcept {
+        return mSpeed;
+    }
+    void Camera2D::setPosition(glm::vec2 pos) noexcept {
+        mPosition = pos;
+    }
+    void Camera2D::setScale(glm::vec2 scale) noexcept {
+        mScale = scale;
+    }
 	void Camera2D::setTargetPosition(glm::vec2 pos) noexcept {
 		mTargetPosition = pos;
-		mFollowMode = Camera2D::FollowMode::FOLLOW_POSITION;
 	}
-	void Camera2D::setFollowLeadSigns(glm::vec2 signs) noexcept {
-		mFollowLeadSigns = signs;
-	}
-	void Camera2D::setFollowSpeed(float speed) noexcept {
-		mFollowSpeed = speed;
-	}
-	void Camera2D::setFollowLead(glm::vec2 lead) noexcept {
-		mFollowLead = lead;
-	}
-	void Camera2D::moveAndLookAt(glm::vec2 where) noexcept {
-		mTargetPosition = where - 0.5f * mScale;
-		mPosition = mTargetPosition;
-		mFollowMode = Camera2D::FollowMode::FOLLOW_POSITION;
-	}
+    void Camera2D::setSpeed(float speed) noexcept {
+        mSpeed = speed;
+    }
+    void Camera2D::setTargetCenter(glm::vec2 where) noexcept {
+        mTargetPosition = where - 0.5f * mScale;
+    }
 	void Camera2D::lookAt(glm::vec2 where) noexcept {
-		mTargetPosition = where - 0.5f * mScale;
-		mFollowMode = Camera2D::FollowMode::FOLLOW_POSITION;
+		mPosition = where - 0.5f * mScale;
 	}
-	Error<Camera2D::FollowState> Camera2D::checkStateTransitions() const noexcept {
-		switch (mFollowState) {
-		case FollowState::IDLE: {
-			if (mFollowMode == FollowMode::FOLLOW_ENTITY) {
-				const ComponentSystem<Draw>& sysDraw = ECS::get().system<Component::draw>();
-				if (!sysDraw.has(mTargetID)) {
-					FATAL(std::format("Target ID {} invalid for Camera2D", mTargetID));
-				}
-				const glm::vec2 targetCenter = sysDraw.position(mTargetID) + 0.5f * sysDraw.scale(mTargetID);
-				const glm::vec2 cameraCenter = mPosition + 0.5f * mScale;
-				const glm::vec2 distance = targetCenter - cameraCenter;
-				if (std::abs(distance.x) > mDeadzone.x or std::abs(distance.y) > mDeadzone.y) {
-					return FollowState::FOLLOWING;
-				}
-			}
-			break;
-		}
-		case FollowState::FOLLOWING: {
-			if (mFollowMode == FollowMode::FOLLOW_ENTITY) {
-				const ComponentSystem<Draw>& sysDraw = ECS::get().system<Component::draw>();
-				const glm::vec2 targetCenter = sysDraw.position(mTargetID) + (0.5f * sysDraw.scale(mTargetID));
-				if (glm::length(targetCenter - getCenter()) <= 0.002f) {
-					return FollowState::IDLE;
-				}
-			}
-			else if (glm::length(mPosition - mTargetPosition) <= 0.002f) {
-				return FollowState::IDLE;
-			}
-			break;
-		}
-        case FollowState::NULL_STATE: std::unreachable();
-		}
-		return FollowState::NULL_STATE;
-	}
-	Error<noreturn> Camera2D::update(float delta) noexcept {
-		// Check state transitions
-		TRY_INIT(FollowState, next, checkStateTransitions());
-		if (next != FollowState::NULL_STATE) {
-			mFollowState = next;
-		}
-		// Do the main update
-		switch (mFollowState) {
-		case FollowState::FOLLOWING: {
-			glm::vec2 target = mTargetPosition;
-			if (mFollowMode == FollowMode::FOLLOW_ENTITY) {
-				const ComponentSystem<Draw>& sysDraw = ECS::get().system<Component::draw>();
-				if (!sysDraw.has(mTargetID)) {
-					FATAL(std::format("Target ID {} invalid for Camera2D", mTargetID));
-				}
-				//Look at the center of the target object
-				target = sysDraw.position(mTargetID) + 0.5f * sysDraw.scale(mTargetID) - 0.5f * mScale;
-			}
-			const glm::vec2 lead = mFollowLead * mFollowLeadSigns;
-			const glm::vec2 distance = target - mPosition + lead;
-			glm::vec2 movement = glm::normalize(distance) * std::min(glm::length(distance), mFollowSpeed * delta);
-			mPosition += movement;
-			break;
-		}
-		case FollowState::IDLE: break;
-        case FollowState::NULL_STATE: std::unreachable();
-		}
+	void Camera2D::update(float delta) noexcept {
+        if (mPosition == mTargetPosition) return;
 
-		return SUCCESS;
+        const glm::vec2 velocityImpact = glm::normalize(mTargetPosition - mPosition) * mSpeed * delta;
+        if (glm::distance(mTargetPosition, mPosition) < glm::length(velocityImpact)) {
+            mPosition = mTargetPosition;
+        }
+        else {
+            mPosition += velocityImpact;
+        }
 	}
 
 	Camera2DBuilder& Camera2DBuilder::setPosition(glm::vec2 pos) noexcept {
@@ -174,30 +57,19 @@ namespace mars {
 		mScale = scale;
 		return *this;
 	}
-	Camera2DBuilder& Camera2DBuilder::setDeadzone(glm::vec2 deadzone) noexcept {
-		mDeadzone = deadzone;
-		return *this;
-	}
 	Camera2DBuilder& Camera2DBuilder::setTargetPosition(glm::vec2 targetPos) noexcept {
 		mTargetPosition = targetPos;
 		return *this;
 	}
-	Camera2DBuilder& Camera2DBuilder::setFollowLead(glm::vec2 lead) noexcept {
-		mFollowLead = lead;
-		return *this;
-	}
-	Camera2DBuilder& Camera2DBuilder::setFollowSpeed(float speed) noexcept {
-		mFollowSpeed = speed;
-		return *this;
-	}
+    Camera2DBuilder& Camera2DBuilder::setSpeed(float speed) noexcept {
+        mSpeed = speed;
+        return *this;
+    }
 	Camera2D Camera2DBuilder::build() const noexcept {
 		Camera2D cam{};
 		cam.mPosition = mPosition;
 		cam.mScale = mScale;
-		cam.mDeadzone = mDeadzone;
 		cam.mTargetPosition = mTargetPosition;
-		cam.mFollowLead = mFollowLead;
-		cam.mFollowSpeed = mFollowSpeed;
 		return cam;
 	}
 }
@@ -225,22 +97,14 @@ Error<mars::Camera2D> JSON::valueTo(const JSON::Value& value) noexcept {
 		TRY_INIT(glm::vec2, scale, JSON::valueTo<glm::vec2>(json.at("scale")));
 		cameraBuilder.setScale(scale * metersPerPixel);
 	}
-	if (json.contains("deadzone")) {
-		TRY_INIT(glm::vec2, deadzone, JSON::valueTo<glm::vec2>(json.at("deadzone")));
-		cameraBuilder.setDeadzone(deadzone * metersPerPixel);
-	}
 	if (json.contains("targetPosition")) {
 		TRY_INIT(glm::vec2, targetPosition, JSON::valueTo<glm::vec2>(json.at("targetPosition")));
 		cameraBuilder.setTargetPosition(targetPosition * metersPerPixel);
 	}
-	if (json.contains("followLead")) {
-		TRY_INIT(glm::vec2, followLead, JSON::valueTo<glm::vec2>(json.at("followLead")));
-		cameraBuilder.setFollowLead(followLead * metersPerPixel);
-	}
-	if (json.contains("followSpeed")) {
-		TRY_INIT(float, followSpeed, JSON::valueTo<float>(json.at("followSpeed")));
-		cameraBuilder.setFollowSpeed(followSpeed * metersPerPixel);
-	}
+    if (json.contains("speed")) {
+        TRY_INIT(float, speed, JSON::valueTo<float>(json.at("speed")));
+        cameraBuilder.setSpeed(speed * metersPerPixel);
+    }
 	return cameraBuilder.build();
 }
 
